007f64944aebdbfa0c7d42ed203912d8a2bc4b23
Gilson Junio Sales Andrade <gilsonjunio@protonmail.com>
2024-06-18 20:36:22 -0300

    ajustei o array de alunos e criei uma função para mapear esse array e apresentar no console, o proximo passo agora é pegar os valores das chaves de cada elemento do array e inserir na estutura da lista de alunos no html

--- js/gerar_alunos.js
+++ js/gerar_alunos.js
@@ -1,54 +1,126 @@
-const alunos = [{
-  "nome":Amazonas,
-  "sigla":AM,
-  "nome":Piauí,
-  "sigla":PI,
-  "nome":Ceará,
-  "sigla":CE,
-  "nome":Distrito Federal,
-  "sigla":DF,
-  "nome":Pernambuco,
-  "sigla":PE,
-  "nome":São Paulo,
-  "sigla":SP,
-  "nome":Goiás,
-  "sigla":GO,
-  "nome":Bahia,
-  "sigla":BA,
-  "nome":Sergipe,
-  "sigla":SE,
-  "nome"Maranhão:,
-  "sigla":MA,
-  "nome":Paraíba,
-  "sigla":PB,
-  "nome":Rio Grande do Norte,
-  "sigla":,
-  "nome":Alagoas,
-  "sigla":AL,
-  "nome":Roraima,
-  "sigla":RR,
-  "nome":Acre,
-  "sigla":AC,
-  "nome":Rondônia,
-  "sigla":RO,
-  "nome":Amapá,
-  "sigla":AM,
-  "nome":Pará,
-  "sigla":PR,
-  "nome":Paraná,
-  "sigla":PA,  
-  "nome":Tocantins,
-  "sigla":TO,  
-  "nome":Mato Grosso do Sul,
-  "sigla":MS,  
-  "nome":Mato Grosso,
-  "sigla":MT,  
-  "nome":Minas Gerais,
-  "sigla":MG,  
-  "nome":Santa Catarina,
-  "sigla":SC,  
-  "nome":Rio Grande do Sul,
-  "sigla":RS,  
-  "nome":Rio de Janeiro,
-  "sigla":RJ,  
-},]
+const lista = document.getElementById("listaAlunos");
+const alunos = [
+  {"nome":"Amazonas","sigla":"AM"
+  },
+  {
+    "nome":"Piauí",
+    "sigla":"PI"
+  },
+  {
+    "nome":"Ceará",
+    "sigla":"CE"
+  },
+  {
+    "nome":"Distrito Federal",
+    "sigla":"DF"
+  },
+  {
+    "nome":"Pernambuco",
+    "sigla":"PE"
+  },
+  {
+    "nome":"São Paulo",
+    "sigla":"SP"
+  },
+  {
+    "nome":"Goiás",
+    "sigla":"GO"
+  },
+  {
+    "nome":"Bahia",
+    "sigla":"BA"
+  },
+  {
+    "nome":"Sergipe",
+    "sigla":"SE"
+  },
+  {
+    "nome":"Maranhão",
+    "sigla":"MA"
+  },
+  {
+    "nome":"Paraíba",
+    "sigla":"PB"
+  },
+  {
+    "nome":"Rio Grande do Norte",
+    "sigla":"RN"
+  },
+  {
+    "nome":"Alagoas",
+    "sigla":"AL"
+  },
+  {
+    "nome":"Roraima",
+    "sigla":"RR"
+  },
+  {
+    "nome":"Acre",
+    "sigla":"AC"
+  },
+  {
+    "nome":"Rondônia",
+    "sigla":"RO"
+  },
+  {
+    "nome":"Amapá",
+    "sigla":"AM"
+  },
+  {
+    "nome":"Pará",
+    "sigla":"PR"
+  },
+  {
+    "nome":"Paraná",
+    "sigla":"PA"
+  },
+  {
+    "nome":"Tocantins",
+    "sigla":"TO"
+  },
+  {
+    "nome":"Mato Grosso do Sul",
+    "sigla":"MS"
+  },
+  {
+    "nome":"Mato Grosso",
+    "sigla":"MT"
+  },
+  {
+    "nome":"Minas Gerais",
+    "sigla":"MG"
+  },
+  {
+    "nome":"Santa Catarina",
+    "sigla":"SC"
+  },
+  {
+    "nome":"Rio Grande do Sul",
+    "sigla":"RS"
+  },
+  {
+    "nome":"Rio de Janeiro",
+    "sigla":"RJ"
+  }];
+
+//Agora vou mapear os elementos do array
+alunos.map( (lista_mapeada) => {
+  console.log(lista_mapeada)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
